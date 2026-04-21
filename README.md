d91f2c983a9b3efb3323d32c830d72c23f60d1ef
Joydeep Gill <joyydeep.gill@gmail.com>
2026-04-21 16:13:28 -0400

    Modifed the README and created base methods for the stock dashboard

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Stoack_Dashboard
+# Stock_Dashboard
 A stock graph for visualizing the price trends for Apple stock for the last few months. 
---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+class Main: 
+
+    def __init__(self): 
+
+    def sanitize_data(self): 
+        ''' 
+        Combing through data from the CSV and extracting it 
+        '''
+
+    def plot_data(self): 
+        ''' 
+        Graphing the extracted data visually 
+        ''' 
+        
