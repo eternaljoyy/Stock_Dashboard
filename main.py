9c8791af3054874fc276e44fed83519834a5b464
Joydeep Gill <joyydeep.gill@gmail.com>
2026-04-21 18:54:13 -0400

    Added functions that plot and sanitize the csv stock data

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class Main: 
-
-    def __init__(self): 
 
     def sanitize_data(self): 
         ''' 
         Combing through data from the CSV and extracting it 
         '''
+        # Read the data from CSV file and extract the price, date of the stock 
+        data_frame = pd.read_csv('APPL_Stocks.csv', usecols=['Open', 'Date'])
 
-    def plot_data(self): 
+        return self.plot_data(data_frame['Open'], data_frame['Date'])
+
+
+    def plot_data(self, prices, date): 
         ''' 
         Graphing the extracted data visually 
         ''' 
-        
+        # From extracted data plot the date (x-axis) and stock price (y-axis) 
+        plt.plot(date, prices) 
+
+        plt.title("APPL Stock Price")
+        # Add the titles and label to the graph 
+        plt.xlabel("Date")
+        plt.ylabel("Open Price")
+
+        #Display the plot 
+        plt.show() 
+
+        #save the plot as a PDF file 
+        return plt.savefig("APPL Stock Prices.pdf")
+    
+
+# Instantiate and run the class 
+if __name__ == "__main__":
+    Main().sanitize_data()
